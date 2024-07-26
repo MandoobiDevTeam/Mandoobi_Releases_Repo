cb792ab677d1a92177d8ec41f9912cb22612ba60
Atef <166168996+M07am2tef@users.noreply.github.com>
2024-07-26 20:41:34 +0300

    Add project files.

---
+++ Mandoobi_Releases_Repo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandoobi_Releases_Repo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
